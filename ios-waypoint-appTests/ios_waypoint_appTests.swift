1edbe57ec73101608988796b3381b27aab57a552
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-03-14 23:49:31 +0200

    Initial Commit

---
+++ ios-waypoint-appTests/ios_waypoint_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_waypoint_appTests.swift
+//  ios-waypoint-appTests
+//
+//  Created by Oleksandr Rolik on 14.03.2025.
+//
+
+import Testing
+@testable import ios_waypoint_app
+
+struct ios_waypoint_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
