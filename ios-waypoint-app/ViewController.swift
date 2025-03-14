623d9f1a09dca43ea74f4c6a3ae644d0d31ad4bf
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-03-15 00:06:09 +0200

    initial commit, implement MapKit

--- ios-waypoint-app/ViewController.swift
+++ ios-waypoint-app/ViewController.swift
@@ -1,19 +1,34 @@
-//
-//  ViewController.swift
-//  ios-waypoint-app
-//
-//  Created by Oleksandr Rolik on 14.03.2025.
-//
-
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    let mapview: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setConstraints()
+        
     }
-
-
 }
+extension ViewController {
+        
+        func setConstraints() {
+            
+            view.addSubview(mapview)
+            NSLayoutConstraint.activate([
+                mapview.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+                mapview.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+                mapview.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+                mapview.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+            ])
+    }
+}
+
 
